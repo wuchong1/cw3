759d4f46a763805dc6d1bbc69a30b0ca9a62709e
wuchong <379412224@qq.com>
2019-06-10 16:57:43 +0800

    xiugai

--- git_demo01/src/main/java/cn/itcast/git/FirstDemo.java
+++ git_demo01/src/main/java/cn/itcast/git/FirstDemo.java
@@ -3,5 +3,15 @@ package cn.itcast.git;
 public class FirstDemo {
     public static void main(String[] args) {
         System.out.println("first git");
+        System.out.println("first git");
+        System.out.println("first git");
+        System.out.println("first git");
+        System.out.println("first git");
+        System.out.println("first git");
+        System.out.println("first git");
+    }
+
+    public void test(){
+        int a = 1;
     }
 }
